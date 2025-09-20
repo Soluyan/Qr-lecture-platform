75c66fa53332afd9d0f0d23bc76be02ce434d2b4
Arlinda Dewey <arlindad@student.21-school.ru>
2025-09-20 18:58:19 +0500

    almost svelte 5 fixed v3

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,2 @@
 import App from './App.svelte';
-
-// Просто создаем приложение без явного указания target
-const app = new App({
-  target: document.getElementById('app')
-});
-
-export default app;
+App({ target: document.body });
